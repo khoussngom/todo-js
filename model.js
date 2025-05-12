35a1c4fc0984893c087a6261fccc417c1c5e0a45
khoussngom <khoussn@gmail.com>
2025-05-12 17:33:17 +0000

    commit1

---
+++ model.js
@@ -0,0 +1,26 @@
+export const model = (() => {
+    let taches = [];
+
+    function ajouterTaches(tache) {
+        if (!tache || tache.trim() === "") return [...taches];
+        taches = [tache, ...taches];
+        return [...taches];
+    }
+
+    function modifierTaches(newtaches, index) {
+        if (!newtaches || isNaN(index) || newtaches.trim() === "" || typeof newtaches !== 'string') return [...taches];
+        taches = taches.map((tache, i) => i === index ? newtaches : tache);
+        return [...taches]
+    }
+
+    function supprimerTaches(index) {
+        taches = taches.filter((_, i) => i !== index);
+        return [...taches]
+    }
+
+    function afficherTaches() {
+        return [...taches]
+    }
+
+    return { ajouterTaches, modifierTaches, supprimerTaches, afficherTaches };
+})();
---
+++ todo.js
@@ -0,0 +1,74 @@
+import { model } from './model.js';
+
+const input = document.querySelector("#inp");
+const ul = document.querySelector("ul");
+const btn = document.querySelector("#btn");
+
+let tache;
+let taches = [];
+let indexchoisi = null;
+
+
+function ajouter(tache) {
+    if (!tache) return;
+
+    if (indexchoisi === null) {
+        taches = model.ajouterTaches(tache);
+    } else {
+        taches = model.modifierTaches(tache, indexchoisi);
+        indexchoisi = null;
+        btn.textContent = "+";
+    }
+}
+
+
+function afficher() {
+    ul.innerHTML = "";
+    taches = model.afficherTaches();
+
+    taches.forEach((tache, index) => {
+        const li = document.createElement("li");
+        li.innerHTML = `
+            <span class="tache">${tache}</span>
+            <span>
+                <button class="modifier"><i class='bx bxs-edit'></i></button>
+                <button class="supprimer"><i class='bx bxs-trash-alt'></i></button>
+            </span>`;
+
+        const btnModifier = li.querySelector(".modifier");
+        const btnSupprimer = li.querySelector(".supprimer");
+
+
+        btnModifier.addEventListener("click", () => {
+            input.value = tache;
+            indexchoisi = index;
+
+        });
+
+
+        btnSupprimer.addEventListener("click", () => {
+            taches = model.supprimerTaches(index);
+            afficher();
+        });
+
+        ul.appendChild(li);
+    });
+}
+
+input.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        ajouter(tache);
+        afficher();
+        input.value = "";
+    }
+});
+
+
+btn.addEventListener("click", function() {
+    ajouter(tache);
+    afficher();
+    input.value = "";
+});
+
+
+afficher();
